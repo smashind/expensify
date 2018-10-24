ee2f692345dbb57907d37e574f575438cb29cfb8
Danny Baumann <dannyjbaumann@gmail.com>
2018-10-24 04:12:43 -0700

    Setup test database environment

--- src/app.js
+++ src/app.js
@@ -5,10 +5,9 @@ import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 import 'react-dates/lib/css/_datepicker.css';
+import './firebase/firebase';
+//import './playground/promises';
 
 const store = configureStore();
 
